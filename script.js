e8bd5819c4c92e3804fb8f837c0914d432f179e3
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-13 13:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // complete the given function
 
 function palindrome(str){
-
+	var s = "";
+	for(let i=0;i<str.length;i++)
+		{
+			if(str.charAt(i)!=' ')
+			{
+				s=s+str.charAt(i);
+			}
+		}
+	let i=0;
+	let j=s.length-1;
+	while(i<j){
+		if(s.charAt(i)!=s.charAt(j))
+		{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
